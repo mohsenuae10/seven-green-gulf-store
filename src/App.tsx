d4dcee1d3ed73d760b285485fbcd8c45c71b2e06
mohsenuae10 <alkurbi2014@hotmail.com>
2026-06-05 07:11:50 -0700

    تحديث

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LanguageProvider from "./providers/LanguageProvider";
 import { CartProvider } from "@/contexts/CartContext";
 import { ChatBot } from "@/components/ChatBot";
@@ -47,7 +47,7 @@ const App = () => (
             <Route path="/compare" element={<Compare />} />
             <Route path="/products" element={<Products />} />
             <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/product" element={<Product />} />
+            <Route path="/product" element={<Navigate to="/products" replace />} />
             <Route path="/order" element={<Order />} />
             <Route path="/payment-success" element={<PaymentSuccess />} />
             <Route path="/privacy" element={<Privacy />} />
